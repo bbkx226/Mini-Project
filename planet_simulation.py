367fce96755bccf21d070ed4296d5103e45e8304
Brandon Ban Kai Xian <83408362+bbkx226@users.noreply.github.com>
2022-05-02 01:39:36 +0800

    Add files via upload

--- planet_simulation.py
+++ planet_simulation.py
@@ -4,7 +4,7 @@ import math
 
 pygame.init()
 
-WIDTH, HEIGHT = 800, 800
+WIDTH, HEIGHT = 1200, 800
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("Planet Simulation by @bbkx")
 
@@ -14,13 +14,16 @@ YELLOW = (255,255,0)
 BLUE = (100,149,237)
 RED = (188,39,50)
 DARK_GREY = (80,78,81)
+PINK_RED = (188,175,178)
+YELLOW_BROWN = (204,153,102)
+BLUE_GREEN = (13,152,186)
 
 FONT = pygame.font.SysFont("comicsans", 16)
 
 class Planet:
     AU = 149.6e6 * 1000  # Astronomical Unit
     G = 6.67428e-11 # Gravitational Constant
-    SCALE = 250 / AU # 1AU = 100 pixels
+    SCALE = 80 / AU # 1AU = 100 pixels
     TIMESTEP = 3600 * 24 # 1 Day
 
     def __init__(self, x, y, radius, color, mass):
@@ -95,14 +98,8 @@ def main():
     run = True
     clock = pygame.time.Clock()
 
-    sun = Planet(0, 0, 30, YELLOW, 1.98892 * 10**30)
+    sun = Planet(0, 0, 20, YELLOW, 1.98892 * 10**30)
     sun.sun = True
-    
-    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
-    earth.y_vel = 29.783 * 1000
-
-    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
-    mars.y_vel = 24.077 * 1000
 
     mercury = Planet(0.387 * Planet.AU, 0, 8, DARK_GREY, 0.330 * 10**24)
     mercury.y_vel = 47.4 * 1000
@@ -110,7 +107,25 @@ def main():
     venus = Planet(0.723 * Planet.AU, 0, 14, WHITE, 4.8685 * 10**24)
     venus.y_vel = -35.02 * 1000
 
-    planets = [sun, earth, mars, mercury, venus]
+    earth = Planet(-1 * Planet.AU, 0, 16, BLUE, 5.9742 * 10**24)
+    earth.y_vel = 29.783 * 1000
+
+    mars = Planet(-1.524 * Planet.AU, 0, 12, RED, 6.39 * 10**23)
+    mars.y_vel = 24.077 * 1000
+
+    jupiter = Planet(-5.2 * Planet.AU, 0, 18, PINK_RED, 1.898 * 10**27)
+    jupiter.y_vel = 13.06 * 1000
+
+    saturn = Planet(-9.5 * Planet.AU, 0, 10, YELLOW_BROWN, 5.683 * 10**26)
+    saturn.y_vel = 9.68 * 1000 
+
+    uranus = Planet(19.8 * Planet.AU, 0, 20, BLUE_GREEN, 8.681 * 10**25)
+    uranus.y_vel = -6.8 * 1000
+    
+    neptune = Planet(30 * Planet.AU, 0, 22, BLUE, 1.024 * 10**26)
+    neptune.y_vel = 5.43 * 1000
+    
+    planets = [sun, earth, mars, mercury, venus, jupiter, saturn, uranus, neptune]
 
     while run:
         clock.tick(60)
